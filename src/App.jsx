3140409822f724993f8afc9ec6470f9fa938b5bb
milanMilicic <milanm.milicic@gmail.com>
2023-12-04 15:48:05 +0100

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './style.scss'
+import { Route, Routes } from "react-router-dom";
 import Navigation from './components/Navigation/Navigation';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <div className='container'>
-        <Navigation />
-      </div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </>
   );
 }
